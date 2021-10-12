183e883297993e183dc9a36f445a600a88b8b456
dsimionescu@totalsoft.ro <dsimionescu@totalsoft.ro>
2021-10-12 13:46:41 +0300

    solution code

---
+++ PaymentGateway.WebApi/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PaymentGateway.WebApi
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddMvc(o => o.EnableEndpointRouting = false);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IConfiguration configuration)
+        {
+            app.UseCors(cors =>
+            {
+                cors
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin();
+            });
+
+#pragma warning disable MVC1005 // Cannot use UseMvc with Endpoint Routing.
+            app.UseMvc();
+#pragma warning restore MVC1005 // Cannot use UseMvc with Endpoint Routing.
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
